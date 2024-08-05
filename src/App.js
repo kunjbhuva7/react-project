43822f5b7b4d2ef3aaa8f0b09a5e01a1948a8be1
kunj <131443874+kunjbhuva7@users.noreply.github.com>
2024-08-05 14:57:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-name: Node.js CI
+import logo from './logo.svg';
+import './App.css';
 
-on:
-  push:
-    branches: [ "master" ]
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+     <p>
+          bhuva <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-jobs:
-  build:
-    runs-on: self-hosted, ubuntu-latest
-
-    strategy:
-      matrix:
-        node-version: [16.x]
-
-    steps:
-    - uses: actions/checkout@v3
-    - name: Use Node.js ${{ matrix.node-version }}
-      uses: actions/setup-node@v3
-      with:
-        node-version: ${{ matrix.node-version }}
-        cache: 'npm'
-    - run: npm ci
-    - run: npm run build --if-present
-    - name: Decode and Save SSH Key
-      run: |
-        echo "${{ secrets.EC2_SSH_KEY }}" | base64 -d > kk.pem || { echo "Failed to decode base64"; exit 1; }
-        chmod 600 kk.pem
-        echo "SSH key length:"
-        wc -c < kk.pem
-    - name: Deploy to EC2
-      env:
-        EC2_HOST: ${{ secrets.EC2_HOST }}
-        EC2_USER: ${{ secrets.EC2_USER }}
-      run: |
-        ssh -v -o StrictHostKeyChecking=no -i kk.pem ${{ secrets.EC2_USER }}@${{ secrets.EC2_HOST }} << 'EOF'
-          cd /home/ubuntu/kunj/testdir
-          exit
-        EOF
+export default App;
